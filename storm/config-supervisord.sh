dc83049537f417f38b78c83e8e8222bb2a01ebb0
Aldwin Barredo <aldwinb@users.noreply.github.com>
2016-05-03 06:27:49 -0400

    Instead of using base image for in storm Dockerfile, moved build steps to Dockerfile.

--- storm/config-supervisord.sh
+++ storm/config-supervisord.sh
@@ -1,5 +1,11 @@
-echo [program:storm-$1] | tee -a /etc/supervisor/conf.d/storm-$1.conf
-echo command=storm $1 | tee -a /etc/supervisor/conf.d/storm-$1.conf
-echo directory=/home/storm | tee -a /etc/supervisor/conf.d/storm-$1.conf
-echo autorestart=true | tee -a /etc/supervisor/conf.d/storm-$1.conf
-echo user=root | tee -a /etc/supervisor/conf.d/storm-$1.conf
+#echo [program:storm-$1] | tee -a /etc/supervisor/conf.d/storm-$1.conf
+#echo command=storm $1 | tee -a /etc/supervisor/conf.d/storm-$1.conf
+#echo directory=/home/storm | tee -a /etc/supervisor/conf.d/storm-$1.conf
+#echo autorestart=true | tee -a /etc/supervisor/conf.d/storm-$1.conf
+#echo user=root | tee -a /etc/supervisor/conf.d/storm-$1.conf
+
+echo [program:storm-$1] | tee -a /etc/supervisor/supervisord.conf
+echo command=storm $1 | tee -a /etc/supervisor/supervisord.conf
+echo directory=/home/storm | tee -a /etc/supervisor/supervisord.conf
+echo autorestart=true | tee -a /etc/supervisor/supervisord.conf
+echo user=root | tee -a /etc/supervisor/supervisord.conf
