3b3b0f32884b35b2d0dc9153fcef833d2c302a17
Aldwin Barredo <aldwinb@users.noreply.github.com>
2016-05-05 20:52:17 -0400

    Changed base to Centos.

--- storm/config-supervisord.sh
+++ storm/config-supervisord.sh
@@ -1,11 +1,5 @@
-#echo [program:storm-$1] | tee -a /etc/supervisor/conf.d/storm-$1.conf
-#echo command=storm $1 | tee -a /etc/supervisor/conf.d/storm-$1.conf
-#echo directory=/home/storm | tee -a /etc/supervisor/conf.d/storm-$1.conf
-#echo autorestart=true | tee -a /etc/supervisor/conf.d/storm-$1.conf
-#echo user=root | tee -a /etc/supervisor/conf.d/storm-$1.conf
-
-echo [program:storm-$1] | tee -a /etc/supervisor/supervisord.conf
-echo command=storm $1 | tee -a /etc/supervisor/supervisord.conf
-echo directory=/home/storm | tee -a /etc/supervisor/supervisord.conf
-echo autorestart=true | tee -a /etc/supervisor/supervisord.conf
-echo user=root | tee -a /etc/supervisor/supervisord.conf
+echo [program:storm-$1] | tee -a /etc/supervisord.d/storm-$1.conf
+echo command=storm $1 | tee -a /etc/supervisord.d/storm-$1.conf
+echo directory=/home/storm | tee -a /etc/supervisord.d/storm-$1.conf
+echo autorestart=true | tee -a /etc/supervisord.d/storm-$1.conf
+echo user=root | tee -a /etc/supervisord.d/storm-$1.conf
--- storm/start-supervisor.sh
+++ storm/start-supervisor.sh
@@ -3,4 +3,4 @@ sed -i -e "s/%nimbus%/$NIMBUS_PORT_6627_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
 
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
 
-/usr/sbin/sshd && supervisord -c "/etc/supervisor/supervisord.conf"
+/usr/sbin/sshd && supervisord
